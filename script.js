c138051f828e83c82c7b959d01bf24a7e63e6a33
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-19 18:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("enterBtn");
+let ptag = document.getElementById("status");
+
+btn.addEventListener("click",()=>{
+	ptag.innerHTML=`<h1>Entered Metaverse</h1>` ;
+})
